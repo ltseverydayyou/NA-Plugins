0d0db616ef8bb8f57cfd39b5fc096f2b7315dbd0
ltseverydayyou <117316014+ltseverydayyou@users.noreply.github.com>
2026-01-12 17:15:24 +0200

    Update doors.lua

--- doors.lua
+++ doors.lua
@@ -1,9 +1,22 @@
+local G = getgenv and getgenv() or _G;
+G.__nadoors = G.__nadoors or {};
+local nd = G.__nadoors;
+local latest = (game:GetService("ReplicatedStorage")).GameData.LatestRoom;
+local rs = game:GetService("RunService");
+local function startNADoors()
+	if nd.conn then
+		return;
+	end;
+	nd.conn = rs.RenderStepped:Connect(function()
+		pcall(function()
+			((((workspace:WaitForChild("CurrentRooms")):WaitForChild(latest.Value)):WaitForChild("Door")):WaitForChild("ClientOpen")):FireServer();
+		end);
+	end);
+end;
 cmdPluginAdd = {
 	{
-		Aliases = {
-			"nadoors"
-		},
-		Info = "Replies with pong",
+		Aliases = {"nadoors","doorsna"},
+		Info = "boop",
 		Function = function()
 			cmdRun("afp goldpile");
 			cmdRun("afp lock");
@@ -33,6 +46,10 @@ cmdPluginAdd = {
 			cmdRun("afp libraryhintpaper");
 			cmdRun("afp crucifix");
 			cmdRun("afp skeletonkey");
+			cmdRun("pesp rushnew");
+			cmdRun("autodel sideroomdupe");
+			cmdRun("ipp");
+			startNADoors();
 		end,
 		RequiresArguments = false
 	}
